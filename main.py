90fc383f15ddc90695ec2ea6fc5e9962f02e3710
Arxilo <matearcila22@gmail.com>
2026-06-04 15:11:25 -0500

    la fase 5 de notificaciones está lista

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-from utils.console import console
-from ai.filter import filtar_ofertas
-from database.db import inicializar_db , guardar_oferta , obtener_por_estado 
+from notifications.whatsapp import WhatsApp
 
-inicializar_db()
-guardar_oferta("Data Analyst Jr", "Google", "Bogotá", "Se busca analista", "https://linkedin.com/job/1", "linkedin")
-filtar_ofertas()
-pendientes = obtener_por_estado("pendiente")
-no_enviar = obtener_por_estado("no_enviar")
+notificador = WhatsApp()
+notificador.send("prueba de AI agent con twilio" , "+573146938467")
 
-console.print("Si enviar: ", pendientes  , style= " bold green")
-console.print("No enviar: ", no_enviar , style= "bold red")
 
 
